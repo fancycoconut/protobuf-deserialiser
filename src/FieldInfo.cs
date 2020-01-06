ee1b23afa0829be96fbeb7263a333988387d1dda
Kawai Wong <kawai.wong@fphcare.co.nz>
2020-01-06 14:01:16 +1300

    Initial Commit

---
+++ src/FieldInfo.cs
@@ -0,0 +1,9 @@
+namespace ProtobufDeserialiser
+{
+    public class FieldInfo
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public FieldDescriptorProto.Types.Type Type { get; set; }
+    }
+}
